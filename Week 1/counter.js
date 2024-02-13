e513c93bac1b8e31a2281088509eb3567f2a9cbf
CaptainSkyFish <ajaystd9756@gmail.com>
2024-02-13 22:42:31 +0530

    counter made

---
+++ Week 1/counter.js
@@ -0,0 +1,12 @@
+var i = 1;
+var limit = 10;
+const counter = () => {
+        console.clear();
+        console.log(i++);
+}
+
+const printCounter = setInterval(counter, 1000);
+
+setTimeout(() => {
+    clearInterval(printCounter);
+}, limit*1000+1000);

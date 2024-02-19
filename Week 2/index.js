893c99efc28e03fca37ef90425f528db782d727d
CaptainSkyFish <ajaystd9756@gmail.com>
2024-02-19 08:59:35 +0530

    express 1.0

--- Week 2/index.js
+++ Week 2/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send(`the sum is: ${sum(10)}`);
+app.get("/", (req, res) => {
+  res.send("HI THERE! WELCOME.")
+})
+
+app.get("/handleSum", (req, res) => {
+  const limit = req.query.limit;
+  res.send(`the sum is: ${sum(limit)}`);
 });
 
 const PORT = process.env.PORT || 3000;

d2dafba71bd236c73335b74f93b9174c18ebb150
CaptainSkyFish <ajaystd9756@gmail.com>
2024-02-20 21:13:06 +0530

    finished todo_list

--- Week 1/todo-list.js
+++ Week 1/todo-list.js
@@ -12,23 +12,39 @@
 */
 
 class Todo {
+  constructor(){
+    this.todo_list = [];
+  }
   add(todo){
-    
+    this.todo_list.push(todo);
+    return this.todo_list;
   }
   remove(indexOfTodo){
-
+    if (indexOfTodo >= 0 && indexOfTodo < this.todo_list.length) {
+      this.todo_list.splice(indexOfTodo, 1);
+    }
+    return this.todo_list;
   }
   update(index, updatedTodo){
-
+    if (index >= 0 && index < this.todo_list.length) {
+      this.todo_list[index] = updatedTodo;
+    }
+    return this.todo_list;
   }
   get(indexOfTodo){
-
+    if (indexOfTodo >= 0 && indexOfTodo < this.todo_list.length) {
+      return this.todo_list[indexOfTodo];
+    } else {
+      // console.log("Invalid index.");
+      return null;
+    }
   }
   getAll(){
-
+    return this.todo_list;
   }
   clear(){
-
+    this.todo_list = [];
+    return this.todo_list;
   }
 }
 
